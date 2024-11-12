31251115b7d60794caabc8ff875d7f7aea86b7c7
dengyifan <dengyifan@genomics.cn>
2024-11-12 15:52:44 +0800

    学习vue

---
+++ bak/04_setup与options app的关系/components/Person.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="person">
+        <h1>姓名：{{ name }}</h1>
+        <h2>年龄: {{ age }}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="showTel">查看联系方式</button>
+        <hr>
+        <h2>测试data:{{ testName }}</h2>
+        <button @click="showTest">查看测试联系方式</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Person',
+        data(){
+            return{
+                testName: 'test_name',
+                testTel: '144454545'
+            }
+        },
+        methods:{
+            showTest(){
+                alert(this.testTel)
+            }
+        },
+        // setup中不能使用this
+        setup(){
+            // 数据
+            let name = 'tom' // 不是响应式
+            let age = 18
+            let tel = '1655468498'
+            // 方法
+            function changeName(){
+                
+                name = 'jack'
+                console.log(name)
+            }
+            function changeAge(){
+                age += 1
+            }
+            function showTel(){
+                alert(tel)
+            }
+            // 交出数据
+            return {name, age, tel, changeName, changeAge, showTel}
+            // return ()=> 'test'
+        }
+    }
+</script>
+
+<style>
+    .person {
+        background-color:blue;
+    }
+    button{
+        margin: 0 10px;
+    }
+</style>
--- bak/05_setup语法糖/components/Person.vue
+++ bak/05_setup语法糖/components/Person.vue
@@ -2,7 +2,7 @@
     <div class="person">
         <h1>姓名：{{ name }}</h1>
         <h2>年龄: {{ age }}</h2>
-        <h3>地址: {{ address }}</h3>
+        <h2>地址: {{ address }}</h2>
         <button @click="changeName">修改名字</button>
         <button @click="changeAge">修改年龄</button>
         <button @click="showTel">查看联系方式</button>
@@ -11,24 +11,22 @@
 </template>
 
 <script lang="ts" setup name="">
-    import {ref} from 'vue'
-    let name = ref('tom') // 不是响应式
-    let age = ref(18)
-    let tel = '1655468498'
-    let address = '中国'
-    // 方法
-    function changeName(){
+    let name = 'tom' // 不是响应式
+            let age = 18
+            let tel = '1655468498'
+            let address = '中国'
+            // 方法
+            function changeName(){
                 
-        name.value = 'jack'
-        console.log(name)
-    }
-    function changeAge(){
-        age.value += 1
-        console.log(age)
-    }
-    function showTel(){
-        alert(tel)
-    }
+                name = 'jack'
+                console.log(name)
+            }
+            function changeAge(){
+                age += 1
+            }
+            function showTel(){
+                alert(tel)
+            }
 </script>
 
 <style>
